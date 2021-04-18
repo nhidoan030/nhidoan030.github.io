9f09820d318bd4df150d2ed31415723d6cfc8670
nhidoan030 <78081099+nhidoan030@users.noreply.github.com>
2021-04-18 10:17:16 +0700

    Update BTJS.js

--- BTJS.js
+++ BTJS.js
@@ -2,9 +2,8 @@
 function showTime() {
     var currentdate = new Date();
     var datetime = currentdate.getDate() + "/" + (currentdate.getMonth()+1) + "/" + currentdate.getFullYear() + " "+ currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds();
-// var datetimeElement = document.getElementById("result1");
-// datetimeElement.innerHTML = datetime;
-    console.log(datetime);
+    var datetimeElement = document.getElementById("result1");
+    datetimeElement.innerHTML = datetime;
 }
 
 //BT2
@@ -18,56 +17,54 @@ function showDatetime() {
         timedate2 = dd + "-" + mm + "-" + currentday.getFullYear();
         dateTime3 = mm + "-" + dd + "-" + currentday.getFullYear();
         timeDate4 = mm + "/" + dd + "/" + currentday.getFullYear();
-//      outPutdate = datetime1 + "     " + timedate2 + "     "+ dateTime3 + "    " + timeDate4;
-//  document.getElementById("result2").innerHTML = outPutdate;
-    console.log(datetime1);
-    console.log(dateTime3);
-    console.log(timeDate4);
-    console.log(timedate2);
+        outPutdate = datetime1 + "     " + timedate2 + "     "+ dateTime3 + "    " + timeDate4;
+    document.getElementById("result2").innerHTML = outPutdate;
 }
 
 
 //BT3
-function isIncrease(numbers) {
-    for (var num = 0; num < numbers.length - 1; num++) {
-        if (!(numbers[num] < numbers[num + 1])) {
-        return false;
+function isIncrease() {
+    var numbers = document.getElementById("ex3-input").value;
+    for (let num = 0; num < numbers.length - 1; num++) {
+        if (numbers[num] < numbers[num + 1]) {
+            alert("True");
         }
+        else alert("False");
     }
-    return true;
 }
 
 //BT4
-function changeLetter(str) {
+function changeLetter() {
+    var str = document.getElementById("ex4-input").value;
     var strArr = str.split("");
     //split: phân tách chuỗi thành mảng
     for(var i=0; i < str.length; i++){
         strArr[i]=String.fromCharCode(str.charCodeAt(i)+1);
     }
-    return strArr.join("");
+    alert(strArr.join(""));
 }
-console.log(changeLetter(str));
+//console.log(changeLetter(str));
 
 //BT5
-function front_between(str) {
-    if (str.length>=3 && str.length%2 == 1) {
+function front_between() {
+    var str = document.getElementById("ex5-input").value;
+    if (str.length >= 3 && str.length %2 == 1) {
         mid = (str.length +1)/2;
-        //slice: trích xuất phần tử giữa (a, b)
-        return str.slice(mid - 2, mid + 1);
-    }
-    return str;
-    }
-console.log(front_between(str));
+        alert(str.slice(mid - 2, mid + 1));
+    } else alert("Invalid");
+}
+//console.log(front_between(str));
 
 //BT6
-function repeatNumber(n) {
+function repeatNumber() {
+    var str = document.getElementById("ex6-input").value;
     var nArr = str.split("");
     var mf = 1;
     var m = 0;
     var item;
     for (var i=0; i< nArr.length; i++)
     {
-        for (var j=i; j < n.length; j++) {
+        for (var j=i; j < nArr.length; j++) {
             if (nArr[i] == nArr[j])
             m++;
             if (mf<m) {
@@ -77,14 +74,17 @@ function repeatNumber(n) {
         }
         m =0;
     }
-    console.log(item) ;
+    alert(item) ;
 }
 
 //BT7
 function startWith(str){
-    return str.indexOf("Java")===0;
+    var str = document.getElementById("ex7-input").value;
+    if (str.indexOf("Java")===0)
+        alert("co" ,startWith(str));
+    else alert ("khong")
 }
-console.log(startWith(str))
+
 
 //BT8
 function callMonth() {
@@ -97,6 +97,7 @@ function callMonth() {
 
 //BT9
 function longestWord(str) {
+    var str = document.getElementById("ex9-input").value;
     var str = str.split(" ");
     var longest = 0;
     var word = null;
@@ -106,14 +107,13 @@ function longestWord(str) {
             word = str[i];
         }
     }
-    return word;
+    alert(word);
 }
 
 //BT10
 function numberBetween() {
-    var lower = Number();
-    var higher = Number();
-    console.log("So nguyen to giua 2 so :");
+    const lower = document.getElementById("ex10-input1").value;
+    const higher =document.getElementById("ex10-input2").value;
     for (let i = lower; i <= higher; i++) {
         let flag = 0;
         for (let j = 2; j < i; j++) {
@@ -123,7 +123,7 @@ function numberBetween() {
             }
         }
         if (i > 1 && flag == 0) {
-            console.log(i);
+            alert(i);
         }
     }
 }
